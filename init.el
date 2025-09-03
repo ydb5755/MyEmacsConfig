e5fefab8e46cadbf9ffbd3545ac2691bfa6cee7b
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-09-03 21:47:15 +0300

    add straight init code

--- init.el
+++ init.el
@@ -11,4 +11,21 @@
 
 (load-theme 'wombat)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (straight-use-package 'magit)
+(straight-use-package 'vertico)
