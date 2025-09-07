7ba29e0ff03dd2e33b27ccc4d2a5913570c71e51
Yisroel Baum <sruli@bhbsdventures.com>
2025-09-07 15:30:41 +0300

    custom saved safe vars

--- init.el
+++ init.el
@@ -42,3 +42,19 @@
 (use-package smartparens
   :config (require 'smartparens-config)
   :init (smartparens-global-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(safe-local-variable-values
+   '((projectile-project-test-cmd . "php ./vendor/bin/phpunit --color=never --verbose"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
